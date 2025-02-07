45bafa520fd453f6bae305e5e35939d190a7545b
Alexandra Fokht <fohtsasha@gmail.com>
2025-02-07 09:37:40 +0100

    Update page.js

--- page.js
+++ page.js
@@ -16,12 +16,7 @@ fromAirportChoice: '//body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/ul[2]/li[2]/a[1
  //Functions
  fillCountry: async function(to) {
 
-    //const fromField = await $(this.fromField);
-    //await fromField.setValue(from);
-    //await fromField.setValue(from);
     await browser.pause(1000);
-    //const fromAirportChoice = await $(this.fromAirportChoice);
-    //fromAirportChoice.selectByVisibleText("Tel aviv, Israel ,All Airports - TLV");
     const toField = await $(this.toField);
     await toField.setValue(to);
     await browser.pause(1000);
@@ -29,8 +24,6 @@ fromAirportChoice: '//body[1]/div[3]/div[2]/div[1]/div[1]/div[1]/ul[2]/li[2]/a[1
     await browser.pause(3000);
     await toAirportChoice.waitForDisplayed();
     toAirportChoice.selectByVisibleText("France ,All Airports - PAR");
-    //const toAirportChoice = await $(this.toAirportChoice);
-    //await toAirportChoice.waitForDisplayed();
     await toAirportChoice.click();
     await browser.pause(3000);
  },
@@ -43,10 +36,7 @@ selectDate: async function() {
     await departDateButton.click();
     await departDateButton.click();
     await browser.pause(1000);
-    //const returnDateButton = await $(this.returnDateButton);
-    //await returnDateButton.waitForDisplayed();
-    //await returnDateButton.click();
-    //await browser.pause(1000);
+
 },
 clickSearchButton: async function () {
     const clickSearchButton = await $(this.searchButton);
